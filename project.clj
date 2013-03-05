1c95c8f4aa51844eef7c60919f437e9f4852e51f
Michael S. Klishin <michael@novemberain.com>
2013-03-05 23:52:56 +0400

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/lein-template "1.3.0"
+(defproject clojurewerkz/lein-template "1.4.0-SNAPSHOT"
   :description "A Leiningen 2.0 template for new ClojureWerkz projects"
   :url "http://github.com/clojurewerkz/lein-template"
   :license {:name "Eclipse Public License"
