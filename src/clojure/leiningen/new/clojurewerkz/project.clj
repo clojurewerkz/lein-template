a0d223adfa6c1486299c8a2d406315c7d2b8c40e
Michael Klishin <michael@defprotocol.org>
2013-03-18 22:19:36 +0400

    Upgrade to Clojure 1.5.1, use 1.5 by default

--- src/clojure/leiningen/new/clojurewerkz/project.clj
+++ src/clojure/leiningen/new/clojurewerkz/project.clj
@@ -1,16 +1,16 @@
 (defproject {{fq-name}} "{{project-version}}"
   :description "FIXME"
   :dependencies [[org.clojure/clojure "{{clojure-version}}"]
-                 [clojurewerkz/support "0.14.0"]]
+                 [clojurewerkz/support "0.15.0"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.6.4"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.5:dev,1.6:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
