a485db248af74f219005b391741ded9ae5fcd788
Michael Klishin <michael@defprotocol.org>
2013-03-18 22:23:51 +0400

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clojurewerkz/lein-template "1.4.0"
+(defproject clojurewerkz/lein-template "1.5.0-SNAPSHOT"
   :description "A Leiningen 2.0 template for new ClojureWerkz projects"
   :url "http://github.com/clojurewerkz/lein-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies  [[clojurewerkz/support "0.14.0"]]
+  :dependencies  [[clojurewerkz/support "0.15.0"]]
   :source-paths  ["src/clojure"]
   :eval-in-leiningen true)
