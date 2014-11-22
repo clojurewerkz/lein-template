98b96b4910a6049ddc65e4049552b92408667ebe
Michael Klishin <michael@clojurewerkz.org>
2014-11-22 18:18:40 +0300

    1.5 is a separate profile

--- src/clojure/leiningen/new/clojurewerkz/project.clj
+++ src/clojure/leiningen/new/clojurewerkz/project.clj
@@ -8,7 +8,7 @@
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.6:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
