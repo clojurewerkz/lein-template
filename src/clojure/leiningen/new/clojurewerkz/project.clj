ce82cc2f918838f3ca3d2f9a402c302bac0d0b77
Michael Klishin <michael@clojurewerkz.org>
2015-12-21 01:05:57 +0300

    Bump to Clojure 1.7

--- src/clojure/leiningen/new/clojurewerkz/project.clj
+++ src/clojure/leiningen/new/clojurewerkz/project.clj
@@ -2,14 +2,12 @@
   :description "FIXME"
   :dependencies [[org.clojure/clojure "{{clojure-version}}"]
                  [clojurewerkz/support "1.1.0"]]
-  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
-             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+  :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC4"]]}
+             :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
-                   :plugins [[codox "0.8.10"]]
-                   :codox {:sources ["src/clojure"]
-                           :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev,master"]}
+                   :plugins [[codox "0.9.0"]]
+                   :codox {:source-paths ["src/clojure"]}}}
+  :aliases {"all" ["with-profile" "dev:dev,1.8:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
